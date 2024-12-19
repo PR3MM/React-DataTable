5d79c4bda1b5d5977019307421f337026b5b7981
Premved Dhote <premveddhote@gmail.com>
2024-12-20 00:13:00 +0530

    bugs fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import BasicDemo from './components/DataTable'
 import 'primereact/resources/themes/lara-light-indigo/theme.css'; 
@@ -9,7 +6,6 @@ import 'primeicons/primeicons.css';
 
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
